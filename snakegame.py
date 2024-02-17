4dc1edd93c1d9e35179a4d208e3c2e769095819a
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-02-17 18:23:20 -0300

    Create snakegame.py

---
+++ snakegame.py
@@ -0,0 +1,26 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+#playground
+pixel_width = 50
+
+snake_pixel = pygame.rect.Rect([0, 0, pixel_width, pixel_width])
+snake = [snake_pixel.copy]
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # RENDER YOUR GAME HERE
